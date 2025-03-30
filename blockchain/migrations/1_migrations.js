f5f10d12c8736dc5c94c900182d7d764eeb784f5
Aniruddha-L <aniruddhanarasimman@gmail.com>
2025-03-30 11:29:05 +0530

    created migration for user

--- blockchain/migrations/1_migrations.js
+++ blockchain/migrations/1_migrations.js
@@ -2,10 +2,12 @@ const disease = artifacts.require('Disease.sol');
 const Hospital = artifacts.require('Hospitals.sol');
 const Ins = artifacts.require('Insurance.sol');
 const terms = artifacts.require('Terms.sol')
+const user = artifacts.require('Users.sol')
 
 module.exports = function(deployer){
     deployer.deploy(disease);
     deployer.deploy(Hospital);
     deployer.deploy(Ins);
     deployer.deploy(terms);
+    deployer.deploy(user);
 }
